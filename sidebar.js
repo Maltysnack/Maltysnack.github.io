a701222991aa1f3bc30ac8d603efa1e70dbeab03
maltysnack <39046911+Maltysnack@users.noreply.github.com>
2026-05-06 18:10:02 +1000

    Flox: add placeholder project page
    New /projects/flox.html with a brief overview of the river-network DSL
    and where the roadmap is taking it. Wired into sidebar.js Projects
    section and index.html home cards. This page will grow into the
    public writeup as Flox progresses.
    
    Co-Authored-By: Wren <wren@maltysnack.dev>

--- sidebar.js
+++ sidebar.js
@@ -100,6 +100,7 @@
             <li><a href="/projects/fantasyfootball.html" ${active('/projects/fantasyfootball.html')}>FPL Predicted XI</a></li>
             <li><a href="/projects/happyhour.html" ${active('/projects/happyhour.html')}>Happy Hour</a></li>
             <li><a href="/projects/worldcup.html" ${active('/projects/worldcup.html')}>World Cup 2026</a></li>
+            <li><a href="/projects/flox.html" ${active('/projects/flox.html')}>Flox</a></li>
           </ul>
         </div>
 
